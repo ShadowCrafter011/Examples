514877ed60ceed4018cfded685200e3b5d8e1173
ShadowCrafter011 <lkoe@bluewin.ch>
2023-10-11 11:23:42 +0200

    Removed console log

--- ImageSwitch/index.js
+++ ImageSwitch/index.js
@@ -10,7 +10,6 @@ image.addEventListener("click", switch_image);
 function switch_image() {
     let current_image = image.getAttribute("src");
     let current_index = images.indexOf(current_image);
-    console.log(current_index)
     if (current_index >= images.length - 1) {
         current_index = 0;
     } else {
